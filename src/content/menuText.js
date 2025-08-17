6eb061241ce84df28c97b2a1310dfd8691177358
JayKhan03721 <jaykhan03721@gmail.com>
2025-08-17 03:00:55 -0400

    Update menuText.js

--- src/content/menuText.js
+++ src/content/menuText.js
@@ -1,11 +1,11 @@
 const english = {
-  title: "Welcome Safraz!",
+  title: "Happy Birthday Safraz!",
   languageIndication: "Press F to switch between French and English",
   playIndication: "Press Enter to start the game",
 };
 
 const french = {
-  title: "Bienvenue, Safraz",
+  title: "&#127881 Bienvenue, Safraz",
   languageIndication:
     "Pesez sur F pour alterner entre le francais et l'anglais",
   playIndication: "Pesez sur la touche Entrer pour commencer le jeu",
