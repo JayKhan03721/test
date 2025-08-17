d8c1ce7cdb82c52eeb0b72812aacc55b828579c6
Jay Khan <jaykhan03721@gmail.com>
2025-08-17 02:23:50 -0400

    first commit

---
+++ src/content/menuText.js
@@ -0,0 +1,19 @@
+const english = {
+  title: "Welcome Safraz!",
+  languageIndication: "Press F to switch between French and English",
+  playIndication: "Press Enter to start the game",
+};
+
+const french = {
+  title: "Bienvenue, Safraz",
+  languageIndication:
+    "Pesez sur F pour alterner entre le francais et l'anglais",
+  playIndication: "Pesez sur la touche Entrer pour commencer le jeu",
+};
+
+const menuText = {
+  english,
+  french,
+};
+
+export default menuText;
